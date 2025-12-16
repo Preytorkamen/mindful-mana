132a9b02d2fe73b7cfdda151877f08a02ca20938
Preytorkamen <preytorkamen@icloud.com>
2025-12-16 10:31:42 -0500

    fix: change bcrypt import to bcryptjs

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import pkg from "pg";
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
 // Helper function for GET /api/sessions/summary for Dashboard
