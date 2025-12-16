30b8743c6d57ad870cb932d01e76eac3ebb0cf47
Preytorkamen <preytorkamen@icloud.com>
2025-12-16 11:25:25 -0500

    fix: use DATABASE_URL for postgres in production

--- server/db.js
+++ server/db.js
@@ -7,11 +7,15 @@ const { Pool } = pg;
 
 // Creating the Pool
 const pool = new Pool ({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,
+    // user: process.env.PG_USER,
+    // host: process.env.PG_HOST,
+    // database: process.env.PG_DATABASE,
+    // password: process.env.PG_PASSWORD,
+    // port: process.env.PG_PORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === "production"
+    ? { rejectUnauthorized: false }
+    : false,
 });
 
 
